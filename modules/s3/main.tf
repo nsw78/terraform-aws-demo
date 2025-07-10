83c66130b9ff25f81d0d561bcf86e331d30f2d69
Nelson <50062575+nsw78@users.noreply.github.com>
2025-07-10 17:09:31 +0000

    Ajustando .gitignore e atualizações nos módulos

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,11 +1,18 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
+  force_destroy = true
+  tags = var.tags
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  tags = {
-    Name = var.bucket_name
-  }
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
 }
